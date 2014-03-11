cfe56fdcd6d3fa0e6c84856fb4bf8b93373b22c8
maxis <maxis1718@gmail.com>
2014-03-11 12:04:49 +0800

    add restore_abbreviation function, modify negation sign and add store_mongo function

--- extract_pattern.py
+++ extract_pattern.py
@@ -19,17 +19,19 @@ co = db['deps']
 # output seperated lists containing each sentence
 def extract_sents(doc):
 	S = defaultdict(list)
-	for x in doc: S[x['usentID']].append(x)
+	for x in doc:
+		S[x['usentID']].append(x)
 	return S.values()
 
-def negation(sent):
+def negation(sent, NEG='__'):
 	negs = [x for x in sent if x['rel'] == 'neg'] # found negation
-	if not negs: return sent
+	if not negs: 
+		return sent
 	for neg in negs:
 		negtarget = (neg['x'], neg['xIdx'])
 		for i, dep in enumerate(sent):
-			if (dep['x'], dep['xIdx']) == negtarget: sent[i]['x'] = '_'+sent[i]['x']
-			if (dep['y'], dep['yIdx']) == negtarget: sent[i]['y'] = '_'+sent[i]['y']
+			if (dep['x'], dep['xIdx']) == negtarget: sent[i]['x'] = NEG+sent[i]['x']
+			if (dep['y'], dep['yIdx']) == negtarget: sent[i]['y'] = NEG+sent[i]['y']
 	return sent
 
 ## input all deps of sents
@@ -57,6 +59,27 @@ def extract_anchors(sent, targets=['VB']):
 					D[pos][anchor].append(dep)
 	return D
 
+abbv = {
+	"'m": "am", 
+	"'s": {'VBP': "is", 'VBD': 'was', 'default': "is"},
+	"'ve": "have",
+	"'ll": "will",
+	"'re": {'VBP': "are", 'VBD': 'were', 'default': 'are'},
+	"'d": {'MD': "would", 'VBD': 'had', 'default': 'would'}
+}
+def restore_abbreviation(abbv, deps):
+	for dep in deps:
+		for xy in ['x', 'y']:
+			w, pos = dep[xy], dep[xy+'Pos']
+			if w in abbv:
+				if type(abbv[w]) == dict: # ambiguous
+					if pos not in abbv[w]:
+						dep[xy] = abbv[w]['default']
+					else:
+						dep[xy] = abbv[w][pos]
+				else:
+					dep[xy] = abbv[w]
+	return deps
 ##
 ## D = 
 ##     {
@@ -72,25 +95,22 @@ def apply_rule(deps, rule):
 
 	D = defaultdict(list)
 	for dep in deps:
-		print '='*30
-		print 'current dep', dep
 		for rel, mincnt in rule:
-			print '\ttest',rel, mincnt,
 			if rel == 'prep' and ('prep' in dep['rel']) and ('_' in dep['rel']): # prepc_without, prep_with
-				print '\tmatch 1'
 				D[dep['rel']].append( dep )
 			elif rel in dep['rel']:
-				print '\tmatch 2'
 				D[rel].append( dep )
 			else:
-				print '\tcontinue'
 				continue
 
+	R = {}
 	## check if match the given rule
 	for rel, mincnt in rule:
-		if len([x for x in D.keys() if rel in x]) < mincnt:
+		occurrence = len([x for x in D.keys() if rel in x])
+		if occurrence < mincnt:
 			return False
-	return dict(D)
+		R[rel] = occurrence
+	return (dict(D), R)
 
 def form(deps, anchor_node):
 	words = set()
@@ -113,7 +133,7 @@ def form(deps, anchor_node):
 def extract_pattern(sent, targets):
 
 	pats = []
-	sent = negation(sent)
+	sent = negation(sent, NEG='__')
 	anchors = extract_anchors(sent, targets)
 
 	for pos in anchors:
@@ -122,15 +142,36 @@ def extract_pattern(sent, targets):
 
 			deps = anchors[pos][anchor_node]
 
-			rels = apply_rule(deps, rule)
-			if not rels: continue
+			# print anchor_node
+
+
+			res = apply_rule(deps, rule)
+			# if not 
+			if not res: 
+				# print '-'*25,'skipped','-'*25
+				continue
+			rels, matched_rule = res
+			# for dep in deps:
+
+				# print dict([(x, dep[x]) for x in dep if x not in ['emotion', 'sent_length', 'udocID', 'usentID']])
+			# (rels)
 
 			combs = ListCombination(rels.values())
+
+			# pprint(combs)
+
 			for comb in combs:
+				# print comb
+				comb = restore_abbreviation(abbv, comb)
+
 				pat = form(comb, anchor_node)
 				weight = 1/float(len(combs))
-				pats.append((pat,weight))
 
+				p = {'anchor':anchor_node, 'pat': pat, 'comb':comb, 'weight':weight, 'matched_rule': matched_rule}
+				# pats.append((pat,weight))
+				pats.append(p)
+				# print pat, weight
+			# print '='*50
 	return pats
 
 def list_possible_rules(rule):
@@ -146,29 +187,85 @@ def list_possible_rules(rule):
 
 	## [(s, 1), (o, 1)]
 	## --> 	s + v + o
-	pass	
+	pass
+
+# {
+#     pattern: "i _love you",
+#     anchor: "love",
+#     anchortype: "verb",
+#     dID: 1,
+#     sID: 2,
+#     vidx: 13,
+#     negation: True,
+#     pLen: 3
+#     sLen: 10
+#     rule: 
+#     {
+#         'subject': 1,
+#         'object' : 1,
+#         'prep'   : 0
+#     }
+# }
+def store_mongo(sent, pats, mongo_collection):
+
+	for p in pats:
+
+		doc = {}
+		doc['sent_length'] = sent[0]['sent_length']
+		doc['udocID'] = sent[0]['udocID']
+		doc['usentID'] = sent[0]['usentID']
+		doc['emotion'] = sent[0]['emotion']		
+
+		doc['pattern_length'] = len(p['pat'])
+		doc['pattern'] = ' '.join([x[0] for x in p['pat']])
+		doc['rule'] = p['matched_rule']
+
+		doc['anchor'] = p['anchor'][0]
+		doc['anchor_type'] = p['anchor'][1]
+		doc['anchor_idx'] = p['anchor'][2]
+
+		doc['weight'] = p['weight']
+		
+		mongo_collection.save(doc)
+
 
 if __name__ == '__main__':
 	
-	udocID = 0
-	rule = [('prep', 0), ('subj',0), ('obj',0), ('cop', 0)]
-	targets = ['VB', 'JJ']
+	display = False
 
-	for udocID in range(10):
+	rule = [('subj',1), ('cop', 1)]
+	# rule = [('prep', 0), ('subj',0), ('obj',0), ('cop', 0)]
+	targets = ['JJ']
+	# targets = ['VB', 'JJ']
 
-		doc = list(co.find({'udocID':udocID}, {'_id':0}))
+	# udocIDs = range(10) # for dev
+	udocIDs = db['deps'].distinct('udocID')
+	MaxudocID = max(udocIDs)
+
+	for udocID in udocIDs:
+
+		doc = list(db['deps'].find( {'udocID':udocID} ))
+
+		## extract all sentences in one document
 		sents = extract_sents(doc)
 
 		for sent in sents:
+
+			## for each sentence, extract patterns
 			pats = extract_pattern(sent, targets)
 
-			sent_str = ' '.join([k[0] for k in sorted(set(reduce(lambda x,y:x+y, [((d['x'],d['xIdx']), (d['y'],d['yIdx'])) for d in sent])), key=lambda a:a[1])])
-			print '> %s (%s)' % (sent_str, color.render( str(len(pats)), 'lc'))
-			for pat,weight in pats:
-				pat_str = ' '.join([x[0] for x in pat])
-				print '  '+color.render(pat_str.lower(), 'g'), round(weight,2)
+			## display results
+			if display:
+				sent_str = ' '.join([k[0] for k in sorted(set(reduce(lambda x,y:x+y, [((d['x'],d['xIdx']), (d['y'],d['yIdx'])) for d in sent])), key=lambda a:a[1])][1:] )
+				print '> %s (%s)' % (sent_str, color.render( str(len(pats)), 'lc'))
+				for p in pats:
+					pat_str = ' '.join([x[0] for x in p['pat']])
+					print '  '+color.render(pat_str.lower(), 'g'), round(p['weight'],2)
 
-		print '%s end of document %d %s' % ('='*20,udocID, '='*20)
-		raw_input()
+			
+			## store back in mongo
+			store_mongo(sent, pats, db['pats'])
 
-
+		print '> %s / %s' % (udocID, MaxudocID )
+		if display:
+			print '%s end of document %d %s' % ('='*20,udocID, '='*20)
