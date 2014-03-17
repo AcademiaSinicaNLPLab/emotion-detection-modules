136a761833b0d53f07037305a2f5822a90ab1159
maxis <maxis1718@gmail.com>
2014-03-17 16:26:41 +0800

    add cache and overwrite option

--- document_scoring.py
+++ document_scoring.py
@@ -10,41 +10,52 @@ co_lexicon = db['lexicon']
 co_patscore = db['patscore']
 co_docscore = db['docscore']
 
+# create a local cache for event_scoring to reduce mongo access times
+cache = {}
+
 ## udocID=1000, emotion='happy'
 ## ds_function=1, opt={'scoring': 1, 'smoothing': 0}, sig_function=0, epsilon=0.5
 def document_scoring(udocID, emotion, ds_function, opt, sig_function, epsilon=0.5):
 
-	s = time.time()
-	mDocs = list( co_pats.find( {'udocID': udocID} ) ) 
-	T['co_pats.find'].append(time.time()-s)
+	# avg: 0.0008 sec 
+	mDocs = list( co_pats.find( {'udocID': udocID} ) )
 
-	
 	# arithmetic mean
 	if ds_function == 1:
-		s = time.time()
-		eventscores = filter( lambda x: x >=0, [event_scoring(pat, emotion, opt, sig_function) for pat in mDocs] )
-		T['event_scoring'].append(time.time()-s)
+		eventscores = filter( lambda x: x >=0, [ event_scoring(pat, emotion, opt, sig_function) for pat in mDocs ] )
 		# all events not in lexicon
 		if len(eventscores) == 0 : 
 			return (None, 0)
 		docscore = sum(eventscores) / float( len(eventscores) )
+
 	# geometric mean
 	elif ds_function == 2:
-		docscore = reduce(lambda x,y:x*y, [event_scoring(pat, emotion, opt, sig_function) for pat in mDocs] )**(1/float(len(a)))
+		docscore = reduce(lambda x,y:x*y, [ event_scoring(pat, emotion, opt, sig_function) for pat in mDocs ] )**(1/float(len(a)))
 	## undefined ds_function
 	else:
 		return False
 	return (docscore, 1) if docscore >= epsilon else (docscore, 0)
 
+
 def event_scoring(pat, emotion, opt, sig_function):
 
-	# build query
+	global cache
+
 	query = {'pattern': pat['pattern'].lower(), 'emotion': emotion}
 	query.update(opt)  # add entries in opt: scoring: 1, smoothing: 0
 	
-	# fetch pattern score from mongo collection "patscore"
-	res = co_patscore.find_one( query )
-	prob_p_e = -1 if not res else res['prob']
+	# form key to access cache
+	key = tuple([query[x] for x in sorted(query.keys())])
+
+	if key not in cache:
+		# fetch pattern score from mongo collection "patscore"
+		res = co_patscore.find_one( query )
+		if not res:
+			cache[key] = -1
+		else:
+			cache[key] = res['prob']
+
+	prob_p_e = cache[key]
 	
 	return pat['weight'] * significance_factor(pat, sig_function) * prob_p_e
 
@@ -58,6 +69,9 @@ def significance_factor(pat, sig_function):
 
 if __name__ == '__main__':
 
+
+	overwrite = False
+
 	ds_function = 1
 	scoring = 1
 	smoothing = 0
@@ -66,16 +80,14 @@ if __name__ == '__main__':
 	epsilon = 0.5
 
 	emotions = [ x['emotion'] for x in co_emotions.find( { 'label': 'LJ40K' } ) ]
-	for gold_emotion in emotions:	
+	for gold_emotion in emotions:
+
 		print gold_emotion
-		docs = list(co_docs.find( { 'emotion': gold_emotion, 'ldocID': {'$gte': 800}} ))
-		for doc in docs:
+		for doc in co_docs.find( { 'emotion': gold_emotion, 'ldocID': {'$gte': 800}} ):
 			_udocID = doc['udocID']
 			for test_emotion in emotions:
 
-				(doc_score, predict) = document_scoring(_udocID, test_emotion, ds_function, opt, sig_function, epsilon)
-
-				query = {
+				Q = {
 						'udocID': _udocID,
 						'gold_emotion': gold_emotion,
 						'test_emotion': test_emotion,
@@ -84,9 +96,26 @@ if __name__ == '__main__':
 						'smoothing':  smoothing,
 						'sig_function': sig_function,
 						'epsilon':  epsilon,
+				}
+				
+				if not overwrite:
+					# check if exist
+					res = co_docscore.find_one(Q)
+					if res:
+						# value already exists, don't do update nor insert
+						continue
+				
+				(doc_score, predict) = document_scoring(_udocID, test_emotion, ds_function, opt, sig_function, epsilon)
+
+				U = {
 						'doc_score':  doc_score,
-						'predict': predict
-					}
+						'predict': predict						
+				}
+				update = { '$set': U }
 
-				co_docscore.insert(d)
+				# Avg: 0.000098 sec
+				# query_for_insert = dict(Q.items() + U.items())
+				# co_docscore.insert(query_for_insert)
 
+				co_docscore.update(Q, update, upsert=True)
+				
