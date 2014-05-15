7f71aa871002353fde09ecbc01f6b31bbef0a4a1
chenyi <chenyi2229@gmail.com>
2014-05-15 16:56:47 +0800

    kf_name -> keywordFeat_name ...

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ import sys, color
 ds_name = 'document_scoring'
 ps_name = 'pattern_scoring'
 ev_name = 'evaluation'
-df_name = 'document_feature'
 genSVM_name = 'toSVM'
 runSVM_name = 'run_svm'
-pf_name = 'pattern_feature'
-kf_name = 'keyword_feature'
+positionFeat_name = 'position_feature'
+patternFeat_name = 'pattern_feature'
+keywordFeat_name = 'keyword_feature'
 
 ## mongo setting
 mongo_addr = 'doraemon.iis.sinica.edu.tw'
@@ -71,10 +71,10 @@ opt_fields = {
 	ps_name: 	['-p','-s','-v', '-o'],
 	ds_name: 	['-p','-d','-s','-g','-l','-v', '-o'],
 	ev_name: 	['-p','-d','-s','-g','-l','-v', '-o'],
-	df_name: 	['-b','-m','-e','-c','-f','-v'],
-
-	pf_name:	['-l','-v'],
-	kf_name:	['-k','--lemma','-v'],
+	
+	positionFeat_name: 	['-b','-m','-e','-c','-f','-v'],
+	patternFeat_name:	['-l','-v'],
+	keywordFeat_name:	['-k','--lemma','-v'],
 
 	genSVM_name:['-v', '-o', '--train', '--test', '--gold', '--root', '--setting'],
 	runSVM_name:['-v', '-o', '--param', '--setting'],
@@ -206,32 +206,6 @@ def help(program, addon=[], exit=1):
 	params['-v'] = [
 		'-v, --verbose: show debug message']
 
-
-	#########################################################################################################
-	## document feature extraction
-	
-	params['-b'] = [
-		'-b, --begPercentage: percentage of beginning section']
-	
-	params['-m'] = [
-		'-m, --midPercentage: percentage of middle section']
-	
-	params['-e'] = [
-		'-e, --endPercentage: percentage of ending section']
-	
-	params['-c'] = [
-		'-c, --countingUnitType: counting unit for document segmentation',
-		'                 0: number of words',
-		'                 1: number of sentences (not implemented yet)']
-	
-	params['-f'] = [
-		'-f, --featureValueType: feature value computation',
-		'                 0: pattern scores (patscore_p2_s0)', 
-		'                 1: accumulated threshold by 0.68 (1 standard diviation) using pattern scores',
-		'                 2: accumulated threshold by 0.68 (1 standard diviation) using pattern occurrence',
-		'                 3: same as type 2 but ignore those with total occurrence < 4 (1, 2, 3)']
-
-	#########################################################################################################
 
 	## add all self-defined option description
 	for k, v in addon:
