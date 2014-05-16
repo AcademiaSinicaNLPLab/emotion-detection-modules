d0d987f754df0e0eeef5232df35230015e7ffbc0
chenyi <chenyi2229@gmail.com>
2014-05-16 11:23:16 +0800

    remove zero count in "remove_self_count()"

--- position_feature.py
+++ position_feature.py
@@ -52,8 +52,10 @@ def remove_self_count(score_dict, udocID):
 	mdoc = co_docs.find_one( {'udocID': udocID} )
 	
 	## ldocID: 0-799	
-	if mdoc['ldocID'] < 800: return score_dict
+	if mdoc['ldocID'] < 800: 
 		score_dict[mdoc['emotion']] = score_dict[mdoc['emotion']] - 1
+		if score_dict[mdoc['emotion']] == 0 :
+			del score_dict[mdoc['emotion']]
 	
 	return score_dict
 
