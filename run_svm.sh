fd8f521bea3ee63a2771f88d313197fd5cf8a2fc
maxis <maxis1718@gmail.com>
2014-05-25 19:43:29 +0800

    add batch run_svm

---
+++ run_svm.sh
@@ -0,0 +1,20 @@
+python run_svm.py 5373115dd4388c6e5d884e49 --param c9r5t1 --quiet	# Accuracy = 3.26756% (260/7957) (classification)
+python run_svm.py 5373109ed4388c55dbf8fc51 --param c9r5t1 --quiet	# Accuracy = 3.63405% (286/7870) (classification)
+python run_svm.py 53730f67d4388c27cc4c06f1 --param c9r5t1 --quiet	# Accuracy = 4.32273% (337/7796) (classification)
+python run_svm.py 537c6be9d4388c5b46f646a6 --param c9r5t1 --quiet	# Accuracy = 4.7816% (370/7738) (classification)
+python run_svm.py 537c6c90d4388c0e27069e7b --param c9r5t1 --quiet	# Accuracy = 5.25834% (402/7645) (classification)
+python run_svm.py 53744c12d4388c206b2cc881 --param c9r5t1 --quiet	# Accuracy = 7.40946% (536/7234) (classification)
+python run_svm.py 53744d55d4388c49faffe9b5 --param c9r5t1 --quiet	# Accuracy = 8.91895% (712/7983) (classification)
+python run_svm.py 53745053d4388c3fc1579c2a --param c9r5t1 --quiet	# Accuracy = 6.92652% (526/7594) (classification)
+python run_svm.py 537451d1d4388c7843516ba4 --param c9r5t1 --quiet	# 
+python run_svm.py 537d838fd4388c3735dc1916 --param c9r5t1 --quiet	# 
+python run_svm.py 537dad77d4388c409cff30bb --param c9r5t1 --quiet	# 
+python run_svm.py 537ec46bd4388c72b5877f67 --param c9r5t1 --quiet	# 
+python run_svm.py 537dd9afd4388c2a1adf8ca2 --param c9r5t1 --quiet	# 
+python run_svm.py 537ddc3ad4388c4fb8dbe1ae --param c9r5t1 --quiet	# 
+python run_svm.py 5378c702d4388c5df3475bee --param c9r5t1 --quiet	# Accuracy = 3.8331% (305/7957) (classification)
+python run_svm.py 53797af2d4388c23183717b4 --param c9r5t1 --quiet	# Accuracy = 3.45608% (275/7957) (classification)
+python run_svm.py 537992d4d4388c22f1b4adbb --param c9r5t1 --quiet	# Accuracy = 3.53148% (281/7957) (classification)
+python run_svm.py 53799c1ed4388c6a604e467a --param c9r5t1 --quiet	# Accuracy = 3.74469% (291/7771) (classification)
+python run_svm.py 5379d6a5d4388c6a09110bb7 --param c9r5t1 --quiet	# Accuracy = 35.577% (2796/7859) (classification)
+python run_svm.py 5379fe4fd4388c110be4cd06 --param c9r5t1 --quiet	# Accuracy = 40.1118% (3085/7691) (classification)
