159ebfbf8fa075f9f128e7057642303a74c23673
Maxis <maxis1718@gmail.com>
2014-05-28 10:45:27 +0800

    remove svm_scale

--- svm_scale.py
+++
@@ -1,7 +0,0 @@
-# scale svm train/test files
-
-# sid = '5384467dd4388c1ca3785cb1'
-sid = '5384154bd4388c0b59bd9b58'
-scale = [0, 10]
-
-# ./svm-scale -l 0 -u 10 5384154bd4388c0b59bd9b58.raw.test > 5384154bd4388c0b59bd9b58.scale10.test
