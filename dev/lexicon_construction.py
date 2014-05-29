e4d00eaadb3a29cbc58ed39924457419587ad118
chenyi <chenyi2229@gmail.com>
2014-05-29 18:19:47 +0800

    move to dev

--- dev/lexicon_construction.py
+++ dev/lexicon_construction.py
@@ -11,9 +11,14 @@ co_lexicon = db['lexicon']
 
 emotions = sorted([x['emotion'] for x in db['emotions'].find({'label':'LJ40K'}, {'_id':0, 'emotion':1})])
 
+patCount = {}
+
+
 # calculate occurrences of patterns
-def count_patterns(emotions):
-	patCount = {}
+def count_patterns():
+
+	global patCount
+
 	for e in emotions:
 		patCount[e] = Counter()
 		for _doc in co_docs.find( { 'emotion': e, 'ldocID': {'$lt': 800}} ):
@@ -21,16 +26,20 @@ def count_patterns(emotions):
 			mdocs = list( co_pats.find( {'udocID': _udocID} ) )
 			for mdoc in mdocs:
 				patCount[e][mdoc['pattern'].lower()] += 1
-	return patCount
+
 
 # build lexicon storing pattern occurrence
-def build_lexicon(patCount, emotions):
+def build_lexicon(patCount):
 	for e in emotions:
 		for p in patCount[e].keys():
 			co_lexicon.insert( { 'pattern': p, 'emotion': e, 'count': patCount[e][p] } )
 
 if __name__ == '__main__':
 
-	count_patterns(emotions)
+	count_patterns()
 
-	build_lexicon(patCount, emotions)
+
+
+	count_patterns()
+
+	build_lexicon(patCount)
