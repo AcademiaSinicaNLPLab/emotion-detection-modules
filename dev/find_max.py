ca835fdd422a2b1d873bb1aedc8fcbbdfdceebf4
Maxis <maxis1718@gmail.com>
2014-05-30 01:29:26 +0800

    add find dimension in find_max; find fusion_id by input

--- dev/find_max.py
+++ dev/find_max.py
@@ -3,8 +3,10 @@ import os
 from collections import Counter
 
 def load(sid, root): return [line.strip().split() for line in open(os.path.join(root, sid+'.train.txt'))]
-def find_local_max(lines): return sorted( [(line[0], max([float(x.split(':')[1]) for x in line[1:]])) for line in lines], key=lambda a:a[1], reverse=True )
-def find_global_max(lines): return max([max([float(x.split(':')[1]) for x in line[1:]]) for line in lines])
+def find_local_max_value(lines): return sorted( [(line[0], max([float(x.split(':')[1]) for x in line[1:]])) for line in lines], key=lambda a:a[1], reverse=True )
+def find_global_max_value(lines): return max([max([float(x.split(':')[1]) for x in line[1:]]) for line in lines])
+
+def find_dimension(lines): return max([max([float(x.split(':')[0]) for x in line[1:]]) for line in lines])
 
 if __name__ == '__main__':
 	
@@ -14,13 +16,16 @@ if __name__ == '__main__':
 	## pat-emo-b (old)
 	# sids = ['537d838fd4388c3735dc1916','5382fa27d4388c23417ddc53']
 
-	sids = ['53855769d4388c4c1f97b796']
+	sids = ['538302743681df11cd509c77']
 
 	root='../tmp'
 
 	for sid in sids:
 		lines = load(sid, root)
-		top_10 = find_local_max(lines)[:10]
+		top_10 = find_local_max_value(lines)[:10]
 		print '='*50
-		print 'Top10 in',sid
+		print 'Top10 value in',sid
 		print top_10
+		print 
+		print 'Dimension:', find_dimension(lines)
+		
