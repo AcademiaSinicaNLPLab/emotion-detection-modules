24e695db4acd07356114fb082b8e5c1622921319
maxis <maxis1718@gmail.com>
2014-06-11 12:01:10 +0800

    Remove extract metadata part

--- extract_dependency.py
+++ extract_dependency.py
@@ -19,7 +19,7 @@ co_sents = None
 co_deps = None
 
 # corpus_root = '/Users/Maxis/corpus/NTCIR/'
-corpus_root = False
+# corpus_root = False
 
 ## sections:
 # default output of Stanford Parser
@@ -33,14 +33,14 @@ corpus_root = False
 	# 	'words': 0,
 	# 	'deps':  1
 	# }
-def process_parsed_files(corpus_root, docIDs, category, sections):
+def process_parsed_files(corpus_root, category, sections):
 
 	usentID = 0
 	## identify sections: words,tree,deps
 	sections = sections.split(',')
 	section_idx = {x:sections.index(x) for x in sections}
 
-	for mdoc in docIDs:
+	for mdoc in co_docs.find():
 
 		fn = mdoc['fn']
 		udocID = mdoc['udocID']
@@ -48,7 +48,7 @@ def process_parsed_files(corpus_root, docIDs, category, sections):
 		categorize_target = mdoc[category]
 		
 		logging.info( 'processing document %s:%s, udocID:%s, ldocID:%s' % (category,categorize_target, udocID, ldocID) )
-		# print 'processing document', category+':', categorize_target, 'udocID:', udocID, 'ldocID:',ldocID
+
 		## load parsed text
 		fpath = os.path.join(corpus_root, fn)
 		doc = open(fpath).read().strip().split('\n\n')
@@ -98,89 +98,9 @@ def process_parsed_files(corpus_root, docIDs, category, sections):
 
 			usentID += 1
 
-def load_NTCIR_docs(corpus_root, category, ext='txt'):
-	udocIDs, ldocIDs, docIDs = {}, Counter(), []
-	# fn:
-	# T=N01.F=1_edn_xxx_20031204_2244111.S=0003.P=POS.txt
-	for fn in os.listdir(corpus_root):
-		if not fn.endswith('.'+ext): continue
-
-		### --------- modify following code to read meta data from filename --------- ###
-
-		# meta: { T: N01, F: ..., S: 0003 }
-		meta = dict([x.split('=') for x in fn.split('.') if '=' in x])
-		categorize_target = meta['P'] ## polarity
-
-
-		## get udocID and ldocID
-		ldocID = ldocIDs[categorize_target] # current file count under __ polarity
-		udocID = len(udocIDs) # current total file count
-		ldocIDs[categorize_target] += 1
-		udocIDs[fn] = True
-
-		doc_meta = {
-
-			## required meta data
-			'ldocID': ldocID,
-			'udocID': udocID,
-			'fn': fn,
-
-			## category: 
-			## e.g., `emotion`, `polarity` or `topic`
-			## change setting in config.py
-			category: categorize_target,
-
-			## additional meta data
-			'filename': meta['F'], 
-			'topic': meta['T'], 
-			'ith-sent': meta['S']
-		}
-		### -------------------------------- (end) --------------------------------- ###
-
-		docIDs.append( doc_meta )
-		co_docs.insert(doc_meta)
-
-	return docIDs
-
-def load_LJ40K_docs():
-	pass
-
-# > db.docs.findOne()
-# {
-	# 	"_id" : ObjectId("53214e23d4388c4792206528"),
-	# 	"emotion" : "crazy",
-	# 	"ldocID" : 0,
-	# 	"udocID" : 19000
-# }
-
-# > db.sents.findOne()
-# {
-	# 	"_id" : ObjectId("531944ac3681dfca09875205"),
-	# 	"emotion" : "accomplished",
-	# 	"sent_length" : 10,
-	# 	"udocID" : 0,
-	# 	"sent_pos" : "I/PRP got/VBD new/JJ hair/NN :/: O/RB omfg/VBG I/PRP love/VBP it/PRP",
-	# 	"usentID" : 0,
-	# 	"sent" : "I got new hair : O omfg I love it"
-# }
-
-# > db.deps.findOne()
-# {
-	# "_id" : ObjectId("531944ac3681dfca098751fc"),
-	# "emotion" : "accomplished",
-	# "sent_length" : 10,
-	# "udocID" : 0,
-	# "xIdx" : 2,
-	# "xPos" : "VBD",
-	# "yPos" : "PRP",
-	# "usentID" : 0,
-	# "rel" : "nsubj",
-	# "y" : "I",
-	# "x" : "got",
-	# "yIdx" : 1
-# }
-
 if __name__ == '__main__':
+
+	program = __file__.split('.py')[0]
 
 	import getopt
 	add_opts = [
@@ -190,12 +110,12 @@ if __name__ == '__main__':
 	try:
 		opts, args = getopt.getopt(sys.argv[1:],'hp:d:ov',['help','path=', 'database=', 'overwrite','verbose'])
 	except getopt.GetoptError:
-		config.help('extract_dependency', addon=add_opts, exit=2)
+		config.help(program, addon=add_opts, exit=2)
 
 	### read options
 	for opt, arg in opts:
-		if opt in ('-h', '--help'): config.help('extract_dependency', addon=add_opts)
-		elif opt in ('-p','--path'): corpus_root = arg.strip()
+		if opt in ('-h', '--help'): config.help(program, addon=add_opts)
+		elif opt in ('-p','--path'): config.corpus_root = arg.strip()
 		elif opt in ('-d','--database'): config.db_name = arg.strip()
 		elif opt in ('-o','--overwrite'): config.overwrite = True
 		elif opt in ('-v','--verbose'): config.verbose = True
@@ -203,21 +123,22 @@ if __name__ == '__main__':
 	loglevel = logging.DEBUG if config.verbose else logging.INFO
 	logging.basicConfig(format='[%(levelname)s] %(message)s', level=loglevel)
 
-	if not corpus_root:
-		logging.error('specify the input corpus path: e.g., python extract_dependency.py -p /corpus/NTCIR/')
+	if not config.corpus_root:
+		logging.error('specify the input corpus path: e.g., python '+program+'.py -p /corpus/NTCIR/')
 		exit(-1)
 
 
 	## use db
 	db = mc[config.db_name]
 
-	## db.<collection_name>
+	## src collection
 	co_docs = db[config.co_docs_name]
+	## dest collections
 	co_sents = db[config.co_sents_name]
 	co_deps = db[config.co_deps_name]
 
 	### check whether destination collection is empty or not
-	dest_cos = [co_docs, co_sents, co_deps]
+	dest_cos = [co_sents, co_deps]
 	dest_cos_status = {co.name : co.count() for co in dest_cos}
 	logging.info('current collection status: ' + json.dumps(dest_cos_status))
 	if sum(dest_cos_status.values()) > 0 and not config.overwrite:
@@ -230,12 +151,9 @@ if __name__ == '__main__':
 		else:
 			for co in dest_cos: co.drop()
 
-	docIDs = load_NTCIR_docs(corpus_root, category=config.category, ext='txt')
-
-	process_parsed_files(corpus_root, docIDs, category=config.category, sections='words,deps')
+	process_parsed_files(corpus_root=config.corpus_root, category=config.category, sections='words,deps')
 
 	logging.info('create index')
-	co_docs.create_index('udocID')
 	co_sents.create_index('udocID')
 	co_deps.create_index('udocID')
 
