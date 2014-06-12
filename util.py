a8befe1eb390f8244609bbb9b0df0d64854e0e50
Maxis <maxis1718@gmail.com>
2014-06-13 01:25:06 +0800

    Move total count --> lexicon_total_count.py

--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import sys
 sys.path.append('pymodules')
 
@@ -88,22 +90,6 @@ def check_indexes(check_list, verbose=True):
 			co.create_index(idx_name)
 			if verbose: logging.warn('create index on %s in %s' % (color.render(idx_name, 'g'), color.render(co.full_name, 'y') ))
 
-def pattern_scoring_function(pat_counts):
-
-	score = {}
-	for anchor in pat_counts:
-
-		SUM = float( sum( [ pat_counts[category] for category in pat_counts if category != anchor ] ) )
-		SUMSQ = float( sum( [ (pat_counts[category] ** 2) for category in pat_counts if category != anchor ] ) )
-		
-		anchor_value = float( pat_counts[anchor] )
-		not_anchor_value = float( SUMSQ/( SUM + 0.9 ** SUM ) )
-		
-		score[anchor] = anchor_value / (anchor_value + not_anchor_value)
-
-	return score
-
-
 ## load entire mongo.LJ40K.docs into memory
 def load_mongo_docs(co_docs):
 	mongo_docs = {}
@@ -118,35 +104,6 @@ def load_mongo_docs(co_docs):
 		mongo_docs = pickle.load(open('cache/'+co_docs.full_name+'.pkl','rb'))
 	return mongo_docs
 
-##  PTC[33680]['i love you']
-##  340 
-def load_lexicon_pattern_total_count(co_ptc, lexicon_type='lexicon'):
-	PatTC = {}
-	# co_ptc = db['lexicon.pattern_total_count']
-	pkl_path = 'cache/PTC.'+lexicon_type+'.pkl'
-	if not os.path.exists(pkl_path):
-		for mdoc in co_ptc.find():
-			PatTC[mdoc['udocID']] = {pat: count for pat, count in mdoc['pats']}
-		if not os.path.exists('cache'): os.mkdir('cache')
-		pickle.dump(PatTC, open(pkl_path,'wb'), protocol=pickle.HIGHEST_PROTOCOL)
-	else:
-		PatTC = pickle.load(open(pkl_path,'rb'))
-	return PatTC
-
-
-## KwTC[0]['bad']
-## 1
-def load_lexicon_keyword_total_count(co_ktc):
-	KwTC = {}
-	# co_ktc = db['lexicon.keyword_total_count']
-	if not os.path.exists('cache/KwTC.lexicon.pkl'):
-		for mdoc in co_ktc.find():
-			KwTC[mdoc['udocID']] = {kw: count for kw, count in mdoc['keywords']}
-		if not os.path.exists('cache'): os.mkdir('cache')
-		pickle.dump(KwTC, open('cache/KwTC.lexicon.pkl','wb'), protocol=pickle.HIGHEST_PROTOCOL)
-	else:
-		KwTC = pickle.load(open('cache/KwTC.lexicon.pkl','rb'))
-	return KwTC
 
 
 
